d5d5d7e19032df348ad84219b7e1cb4ae2c3ba64
Stefan Eng <stefaneng13@gmail.com>
2016-02-07 09:53:26 -0800

    Add requests

--- sen.py
+++ sen.py
@@ -2,44 +2,15 @@ import os
 import requests
 import RPi.GPIO as GPIO                    #Import GPIO library
 import time                                #Import time library
-GPIO.setmode(GPIO.BCM)                     #Set GPIO pin numbering
+GPIO.setmode(GPIO.BCM)                     #Set GPIO pin numbering 
 
 TRIG = 23                                  #Associate pin 23 to TRIG
 ECHO = 24                                  #Associate pin 24 to ECHO
-previous_distance=[]
-trash_full=False
+previous_distance=0
+garbage_in_trash=0
+garbage_full=0
+counter=0
 host= os.getenv('TRASH_HOST', 'http://localhost:3000')
-
-def mean(data):
-    """Return the sample arithmetic mean of data."""
-    n = len(data)
-    if n < 1:
-        raise ValueError('mean requires at least one data point')
-    return sum(data)/n # in Python 2 use sum(data)/float(n)
-
-def _ss(data):
-    """Return sum of square deviations of sequence data."""
-    c = mean(data)
-    ss = sum((x-c)**2 for x in data)
-    return ss
-
-def pstdev(data):
-    """Calculates the population standard deviation."""
-    n = len(data)
-    if n < 2:
-        raise ValueError('variance requires at least two data points')
-    ss = _ss(data)
-    pvar = ss/n # the population variance
-    return pvar**0.5
-
-def isStableDistance(data):
-  """
-  Returns true if distance for the data is 'stable'. That is, std is < 1
-  """
-  return pstdev(data) < 1
-
-def distanceInRange(dist):
-  return dist > 4 and dist < 30
 
 GPIO.setup(TRIG,GPIO.OUT)                  #Set pin as GPIO out
 GPIO.setup(ECHO,GPIO.IN)                   #Set pin as GPIO in
@@ -47,7 +18,7 @@ GPIO.setup(ECHO,GPIO.IN)                   #Set pin as GPIO in
 while True:
 
   GPIO.output(TRIG, False)                 #Set TRIG as LOW
-  time.sleep(.25)                          #Delay of 2 seconds
+  time.sleep(2)                            #Delay of 2 seconds
 
   GPIO.output(TRIG, True)                  #Set TRIG as HIGH
   time.sleep(0.00001)                     #Delay of 0.00001 seconds
@@ -57,25 +28,36 @@ while True:
     pulse_start = time.time()              #Saves the last known time of LOW pulse
 
   while GPIO.input(ECHO)==1:               #Check whether the ECHO is HIGH
-    pulse_end = time.time()                #Saves the last known time of HIGH pulse
+    pulse_end = time.time()                #Saves the last known time of HIGH pulse 
 
   pulse_duration = pulse_end - pulse_start #Get pulse duration to a variable
 
   distance = pulse_duration * 17150        #Multiply pulse duration by 17150 to get distance
   distance = round(distance, 2)            #Round to two decimal points
 
-  if len(previous_distance) > 5 and isStableDistance(previous_distance) and distanceInRange(distance):
-    if not trash_full:
-      trash_full = True
-      r = requests.post(host + "/api/full/trashsensor")
-      print "Trashcan full", r
+  if (previous_distance + 4) <= distance or (previous_distance - 4) >= distance:
+    garbage_in_trash=1
+    garbage_full=1
+    counter=0
   else:
-    if trash_full:
-      trash_full = False
-      r = requests.post(host + "/api/empty/trashsensor")
-      print "Trashcan emptied", r
+    counter=counter+1	
 
-  # Keep track of the last 10 items to keep the stdev
-  previous_distance.append(distance)
-  if len(previous_distance) > 9:
-    previous_distance = previous_distance[1:]
+  previous_distance=distance
+
+  if garbage_in_trash==1:
+    print "Trash was just put in" 
+    garbage_in_trash=0	
+ 	
+  if garbage_full==1 and counter==4:
+    r = requests.post(host + "/api/full/trashsensor")
+    print "Take out the trash bruh!!!", r.text
+    counter=0
+    garbage_full=0
+  r = requests.post(host + "/api/empty/trashsensor")
+  print "empty request", r.text
+  if distance > 2 and distance < 400:      #Check whether the distance is within range
+    print "Distance:",distance - 0.5,"cm"  #Print distance with 0.5 cm calibration
+  else:
+    print "Out Of Range"                   #display out of range
+
+  
